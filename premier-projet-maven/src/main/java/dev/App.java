8ccbb081e983aa594bbef4eaa55c8d30ec911198
Xavier CALLOCH <callochxavier@gmail.com>
2020-02-26 18:04:14 +0100

    7e commit

--- premier-projet-maven/src/main/java/dev/App.java
+++ premier-projet-maven/src/main/java/dev/App.java
@@ -1,5 +1,6 @@
 package dev;
 
+import java.util.ResourceBundle;
 import com.github.lalyos.jfiglet.FigletFont;
 
 /**
@@ -7,19 +8,14 @@ import com.github.lalyos.jfiglet.FigletFont;
  *
  */
 
-/**
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
-}
-*/
-
 public class App {
 	public static void main(String[] args) {
-		String asciiArt = FigletFont.convertOneLine("App");
+		String titre = ResourceBundle.getBundle("application").getString("titre");
+		String asciiArt = FigletFont.convertOneLine(titre);
 	    System.out.println(asciiArt);
+	    
+	    // affichage de l'environnement
+	    String environnement = ResourceBundle.getBundle("application").getString("environnement");
+	    System.out.println("Environnement : " + environnement);
 	}
 }
